3e4f32da14c07b06a91b50b02a90d61979d30ae5
korolenkoelizaveta <korolenkoelizaveta1@gmail.com>
2025-02-15 21:48:50 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,41 +1,53 @@
-﻿using System;
+using System;
 
 class First
 {
+    public class Logic {
+        public static int[] Compare(int a, int b, int c) {
+            int[] nums = new int[3];
+            if (a >= b && a >= c)
+                nums[0] = a;
+            else if (b >= a && b >= c)
+                nums[0] = b;
+            else
+                nums[0] = c;
+
+            if (a <= b && a <= c)
+                nums[1] = a;
+            else if (b <= a && b <= c)
+                nums[1] = b;
+            else
+                nums[1] = c;
+
+            if (a != nums[0] && a != nums[1])
+                nums[2] = a;
+            else if (b != nums[0] && b != nums[1])
+                nums[2] = b;
+            else nums[2] = c;
+
+            return nums;
+        }
+    }
     static void Main()
     {
+        // НАЧАЛО взаимодействия с пользователем
         Console.Write("Введите первое число: ");
         int a = Convert.ToInt32(Console.ReadLine());
         Console.Write("Введите второе число: ");
         int b = Convert.ToInt32(Console.ReadLine());
         Console.Write("Введите третье число: ");
         int c = Convert.ToInt32(Console.ReadLine());
+        // КОНЕЦ взаимодействия с пользователем
 
-        int ladge;
-        if (a >= b && a >= c)
-            ladge = a;
-        else if (b >= a && b >= c)
-            ladge = b;
-        else
-            ladge = c;
+        // НАЧАЛО логики
+        int[] nums = Logic.Compare(a, b, c);
+        // КОНЕЦ логики
 
-        int small;
-        if (a <= b && a <= c)
-            small = a;
-        else if (b <= a && b <= c)
-            small = b;
-        else
-            small = c;
-
-        int sred;
-        if (a != ladge && a != small)
-            sred = a;
-        else if (b != ladge && b != small)
-            sred = b;
-        else sred = c;
-
-        Console.WriteLine($"Самое большое число: {ladge}");
-        Console.WriteLine($"Самое маленькое число: {small}");
-        Console.WriteLine($"Самое среднее число: {sred}");
+        // НАЧАЛО взаимодействия с пользователем
+        Console.WriteLine($"Самое большое число: {nums[0]}");
+        Console.WriteLine($"Самое маленькое число: {nums[1]}");
+        Console.WriteLine($"Самое среднее число: {nums[2]}");
+        // КОНЕЦ взаимодействия с пользователем
     }
 }
+

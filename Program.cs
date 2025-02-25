987f330c5d9850b78c6c5ac0077e9ecab3176825
korolenkoelizaveta <korolenkoelizaveta1@gmail.com>
2025-02-25 21:09:17 +0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,21 @@
-using System;
-
-class First
+/*
+ Задача №10. Даны три различных целых числа. Определить, какое из них
+(первое, второе или третье): а) самое большое; б) самое маленькое; 
+в) является средним (средним назовем число, которое больше наименьшего
+из данных чисел, но меньше наибольшего).
+ */
+namespace Lab1task1;
+public class First
 {
-    public class Logic {
-        public static int[] Compare(int a, int b, int c) {
+    public class Logic
+    {
+        public static int[] Compare(int a, int b, int c)
+        {
             int[] nums = new int[3];
+
+            if (a == b && b == c)
+                return new int[] { int.MinValue, int.MinValue, int.MinValue };
+
             if (a >= b && a >= c)
                 nums[0] = a;
             else if (b >= a && b >= c)
@@ -19,11 +30,14 @@ class First
             else
                 nums[1] = c;
 
-            if (a != nums[0] && a != nums[1])
+            if ((a == b && b == c) || (a == b) || (a == c) || (b == c))
+                nums[2] = int.MinValue; 
+            else if (a != nums[0] && a != nums[1])
                 nums[2] = a;
             else if (b != nums[0] && b != nums[1])
                 nums[2] = b;
-            else nums[2] = c;
+            else
+                nums[2] = c;
 
             return nums;
         }
@@ -44,9 +58,17 @@ class First
         // КОНЕЦ логики
 
         // НАЧАЛО взаимодействия с пользователем
-        Console.WriteLine($"Самое большое число: {nums[0]}");
-        Console.WriteLine($"Самое маленькое число: {nums[1]}");
-        Console.WriteLine($"Самое среднее число: {nums[2]}");
+        if (nums[0] == int.MinValue && nums[1] == int.MinValue && nums[2] == int.MinValue)
+            Console.WriteLine("Все числа одинаковые");
+        else
+        {
+            Console.WriteLine($"Самое большое число: {nums[0]}");
+            Console.WriteLine($"Самое маленькое число: {nums[1]}");
+            if (nums[2] == int.MinValue)
+                Console.WriteLine("Среднее число отсутствует");
+            else
+                Console.WriteLine($"Среднее число: {nums[2]}");
+        }
         // КОНЕЦ взаимодействия с пользователем
     }
 }
